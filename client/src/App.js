b5665bde76164cee1bbc1eb8857f42b2a846de79
Max Skala <max.skala@odysys.com>
2017-01-19 16:32:29 -0500

    Connect client to server

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,18 @@
 import React, { Component } from 'react';
+import Client from './Client';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {testResult: 'pending'};
+  };
+
+  componentDidMount() {
+    Client.test((response) => this.setState({testResult: response.test}));
+  };
+
   render() {
     return (
       <div className="App">
@@ -13,6 +23,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>Test result: {this.state.testResult}</p>
       </div>
     );
   }
--- server.js
+++ server.js
@@ -4,9 +4,14 @@ const app = express();
 app.set('port', (process.env.PORT || 3001));
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
+   app.use(express.static('client/build'));
 }
 
 app.get('/api/test', (req, res) => {
-    res.json({test: 'successful'});
+  res.json({test: "successful"});
+});
+
+
+app.listen(app.get('port'), () => {
+  console.log(`Find the server at: http://localhost:${app.get('port')}/`);
 });
