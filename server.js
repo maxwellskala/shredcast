a3111b6c49a6e3dfa7da6b9c045e3659743f42ae
Max Skala <max.skala@odysys.com>
2017-01-20 16:02:58 -0500

    Log possible db connection errors

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ if (process.env.NODE_ENV === 'production') {
 
 app.get('/api/test', (req, res) => {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+  	console.log(err);
     client.query('SELECT * FROM test_table', function(err, result) {
       done();
       if (err) {
