72e5e3b42fbe134acc89ecfc090cf719e9c627cb
Max Skala <max.skala@odysys.com>
2017-01-29 15:51:44 -0500

    Have client respond to logging in or signing up

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,129 +1,45 @@
 import React, { Component } from 'react';
 import Client from '../data/Client';
+import SignupLoginForm from './SignupLoginForm';
 import logo from '../assets/logo.svg';
 import './App.css';
 
-const SIGNUP = 'signup';
-const LOGIN = 'login';
-const EMAIL = 'email';
-const PASSWORD = 'password';
-
 class App extends Component {
-  // @TODO refactor rendering/handling of signup/login to share code better
   constructor(props) {
     super(props);
     this.state = {
       testResult: 'pending',
-      [SIGNUP]: {
-        [EMAIL]: '',
-        [PASSWORD]: ''
-      },
-      [LOGIN]: {
-        [EMAIL]: '',
-        [PASSWORD]: ''
-      },
       user: null
     };
 
     // @TODO refactor this garbage once class properties are legit
-    this.handleSignup = this.handleSignup.bind(this);
-    this.handleLogin = this.handleLogin.bind(this);
-    this.renderSignup = this.renderSignup.bind(this);
-    this.renderLogin = this.renderLogin.bind(this);
+    this.handleReceiveUser = this.handleReceiveUser.bind(this);
+    this.renderUserBody = this.renderUserBody.bind(this);
   };
 
   componentDidMount() {
     Client.test((response) => this.setState({testResult: response.test}));
   };
 
-  handleChange(stateKey, fieldKey) {
-    return (e) => {
-      this.setState({
-        ...this.state,
-        [stateKey]: {
-          ...this.state[stateKey],
-          [fieldKey]: e.target.value
-        }
-      });
-    };
+  handleReceiveUser(response) {
+    const user = response.user;
+    this.setState({ user });
   };
 
-  handleSignup(e) {
-    e.preventDefault();
-    Client.signup(
-      this.state[SIGNUP][EMAIL],
-      this.state[SIGNUP][PASSWORD],
-      (response) => console.log(response, 'from backend, signup')
-    );
-  };
-
-  handleLogin(e) {
-    e.preventDefault();
-    Client.login(
-      this.state[LOGIN][EMAIL],
-      this.state[LOGIN][PASSWORD],
-      (response) => console.log(response, 'from backend, login')
-    );
-  };
-
-  renderSignup() {
-    if (this.state.user !== null) {
-      return null;
+  renderUserBody() {
+    const { user } = this.state;
+    if (user === null) {
+      return (
+        <SignupLoginForm
+          onLogin={Client.login}
+          onSignup={Client.signup}
+          onReceiveUser={this.handleReceiveUser}
+        />
+      );
+    } else if (!user.email) {
+      return <div>You're logged in as an unknown user, WTF!</div>;
     }
-    return (
-      <div className="signup-container">
-        <h3>Sign up</h3>
-        <form className="signup-form" onSubmit={this.handleSignup}>
-          <label>
-            Email:
-            <input
-              type="text"
-              value={this.state[SIGNUP][EMAIL]}
-              onChange={this.handleChange(SIGNUP, EMAIL)}
-            />
-          </label>
-          <label>
-            Password:
-            <input
-              type="text"
-              value={this.state[SIGNUP][PASSWORD]}
-              onChange={this.handleChange(SIGNUP, PASSWORD)}
-            />
-          </label>
-          <input type="submit" value="Submit" />
-        </form>
-      </div>
-    );
-  };
-
-  renderLogin() {
-    if (this.state.user !== null) {
-      return null;
-    }
-    return (
-      <div className="login-container">
-        <h3>Login</h3>
-        <form className="login-form" onSubmit={this.handleLogin}>
-          <label>
-            Email:
-            <input
-              type="text"
-              value={this.state[LOGIN][EMAIL]}
-              onChange={this.handleChange(LOGIN, EMAIL)}
-            />
-          </label>
-          <label>
-            Password:
-            <input
-              type="text"
-              value={this.state[LOGIN][PASSWORD]}
-              onChange={this.handleChange(LOGIN, PASSWORD)}
-            />
-          </label>
-          <input type="submit" value="Submit" />
-        </form>
-      </div>
-    );
+    return <div>Congrats! You are logged in with email {user.email} </div>;
   };
 
   render() {
@@ -137,8 +53,7 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
         <p>Test result: {this.state.testResult}</p>
-        {this.renderSignup()}
-        {this.renderLogin()}
+        {this.renderUserBody()}
       </div>
     );
   }
---
+++ client/src/components/SignupLoginForm.js
@@ -0,0 +1,93 @@
+import React, { Component } from 'react';
+import './SignupLoginForm.css';
+
+const EMAIL = 'email';
+const PASSWORD = 'password';
+
+class SignupLoginForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showLogin: true,
+      [EMAIL]: '',
+      [PASSWORD]: ''
+    };
+
+    this.getFormToggleText = this.getFormToggleText.bind(this);
+    this.handleFormToggle = this.handleFormToggle.bind(this);
+  };
+
+  getFormToggleText(showLogin) {
+    const className = 'toggle-form-text';
+    return showLogin
+      ? (
+          <p className={className}>
+            Don't have an account? <a onClick={this.handleFormToggle}>Signup</a>
+          </p>
+        )
+      : (
+          <p className={className}>
+            Already have an account? <a onClick={this.handleFormToggle}>Login</a>
+          </p>
+        );
+  };
+
+  getHandleChange(stateKey) {
+    return (e) => {
+      return this.setState({ [stateKey]: e.target.value });
+    };
+  };
+
+  handleFormToggle() {
+    this.setState((prevState, props) => {
+      return { ...prevState, showLogin: !prevState.showLogin };
+    })
+  };
+
+  render() {
+    const { onLogin, onSignup, onReceiveUser } = this.props;
+    const { showLogin, email, password } = this.state;
+    const clientHook = showLogin ? onLogin : onSignup;
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      clientHook(
+        email,
+        password,
+        onReceiveUser
+      );
+    };
+    return (
+      <div className='signup-login-container'>
+        <h3>{showLogin ? 'Login' : 'Signup'}</h3>
+        {this.getFormToggleText(showLogin)}
+        <form className='signup-login-form' onSubmit={handleSubmit}>
+          <label>
+            Email:
+            <input
+              type='text'
+              value={email}
+              onChange={this.getHandleChange(EMAIL)}
+            />
+          </label>
+          <label>
+            Password:
+            <input
+              type='text'
+              value={password}
+              onChange={this.getHandleChange(PASSWORD)}
+            />
+          </label>
+          <button type='submit' value='Submit'>Submit</button>
+        </form>
+      </div>
+    );
+  };
+}
+
+SignupLoginForm.propTypes = {
+  onLogin: React.PropTypes.func.isRequired,
+  onSignup: React.PropTypes.func.isRequired,
+  onReceiveUser: React.PropTypes.func.isRequired
+}
+
+export default SignupLoginForm;
