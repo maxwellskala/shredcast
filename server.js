4b7249d3ff93800f7c79bce7eca71b5c3c85e0b0
Max Skala <max.skala@odysys.com>
2017-01-29 21:14:26 -0500

    Maybe leave session cookie settings to default will fix Chrome in prod

--- server.js
+++ server.js
@@ -61,8 +61,7 @@ app.use(session({
   }),
   secret: 'mysecret',
   resave: false,
-  saveUninitialized: false,
-  cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 }
+  saveUninitialized: false
 }));
 app.use(passport.initialize());
 app.use(passport.session());
