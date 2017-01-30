5b6d3bf35303afb930401893e29598cfff1f7867
Max Skala <max.skala@odysys.com>
2017-01-30 11:53:45 -0500

    Add status codes to routes

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,12 @@
 exports.checkSession = (req, res) => {
   if (!req.user) {
-    return res.json({ user: false });
+    return res
+      .status(204)
+      .json({ user: false });
   } else {
-    return res.json({ user: req.user });
+    return res
+      .status(200)
+      .json({ user: req.user });
   }
 };
 
@@ -12,15 +16,19 @@ exports.signup = (User) => (req, res) => {
     email,
     password
   })
-  .then((user) => res.json({ user }))
-  .catch((err) => res.json({ err }));
+  .then((user) => res.status(201).json({ user }))
+  .catch((err) => res.status(500).json({ err }));
 };
 
 exports.login = (req, res) => {
-  return res.json({ user: req.user });
+  return res
+    .status(200)
+    .json({ user: req.user });
 };
 
 exports.logout = (req, res) => {
   req.logout();
-  return res.json({ success: 'Logged out!' });
+  return res
+    .status(204)
+    .json({ success: 'Logged out!' });
 }
