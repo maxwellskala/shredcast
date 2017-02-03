376dda6423d0ffcfea5ce9a05866b2d45e8cf017
Max Skala <max.skala@odysys.com>
2017-02-03 15:20:29 -0500

    Use db/config to connect to database in server.js

---
+++ db/config/index.js
@@ -0,0 +1,13 @@
+const config = require('./config');
+
+const configureDb = (nodeEnv) => {
+  const envConfig = config[nodeEnv];
+  const maybeEnvVariable = envConfig.use_env_variable;
+  if (maybeEnvVariable) {
+    return process.env[use_env_variable];
+  }
+  const { username, password, database, host } = envConfig;
+  return `postgres://${username}:${password}@${host}/${database}`;
+};
+
+module.exports = configureDb;
