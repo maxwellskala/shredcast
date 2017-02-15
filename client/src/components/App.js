51fa01faaf7b75ef011f76603d8472b2cb7449bb
Max Skala <max.skala@odysys.com>
2017-02-15 12:31:52 -0500

    Scope App CSS to component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -95,11 +95,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+        <div className="header">
+          <img src={logo} className="logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
+        <p className="intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
         {this.renderUserBody()}
